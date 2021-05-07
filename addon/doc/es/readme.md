230f1486a4ad95da38786376b656c6e8c510b89c
Gerardo Késsler <gera.kessler@gmail.com>
2021-05-07 15:02:33 -0300

    Corregida la documentación. Agregada addon template

---
+++ addon/doc/es/readme.md
@@ -0,0 +1,37 @@
+# WhatsApp desktop:
+Este complemento ha sido desarrollado para facilitar el uso de la aplicación agregando atajos de teclado para algunas funciones importantes, así como etiquetando algunos botones que no cuentan con nombre alguno.
+
+<br>
+
+## Instrucciones y lista de comandos:
+Al abrir WhatsApp desktop automáticamente va a activarse el modo foco. Pulsando shift tabulador luego de que termine de cargar la aplicación, colocará el foco en la lista de chats que podremos recorrer con flechas arriba y abajo.  
+nota: Este complemento solo funciona con el modo foco activo.  
+
+<br>
+
+### Atajos del complemento:
+
+* Iniciar y detener la grabación de un mensaje de voz; control + r.
+* Copiar el texto del mensaje con el foco; control + shift + c.
+* Abrir el link del mensaje con el foco en el navegador por defecto; control + l.
+* Abrir el menú adjuntar; control + shift + a. Solo disponible desde el cuadro de edición de mensaje.
+* Abrir el menú del chat; control + m.
+* Abrir el menú general de WhatsApp; control + g. Solo disponible desde la lista de mensajes de un chat.
+* En aleatorias actualizaciones de la aplicación se suele romper la acción de reproducir los mensajes de voz con la barra espaciadora, por lo que el complemento agrega esta opción pulsando intro sobre el mensaje para cuando esta no funcione.
+* Descargar el archivo del mensaje cuando el mismo contiene alguno; alt + intro.
+* Mover el foco al mensaje respondido. shift + intro. Solo disponible desde mensajes de voz.
+
+<br>
+
+### Atajos generales de la aplicación:
+
+* Crear nuevo chat; control + n.
+* Activar el cuadro de búsqueda de chats; control + f.
+* Archivar un chat; control + e.
+* Menú contextual del chat; flecha derecha.
+* Enfocar el chat siguiente; control + tabulador.
+* Enfocar el chat anterior; control + shift + tabulador.
+* Borrar el chat; control + shift + d.
+* Fijar, desfijar chat; control + shift + p.
+* Abrir el menú contextual del mensaje; flecha derecha
+* Activar la búsqueda de mensajes del chat; control + shift + f.
---
+++ buildVars.py
@@ -0,0 +1,56 @@
+﻿# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+
+# Since some strings in `addon_info` are translatable,
+# we need to include them in the .po files.
+# Gettext recognizes only strings given as parameters to the `_` function.
+# To avoid initializing translations in this module we simply roll our own "fake" `_` function
+# which returns whatever is given to it as an argument.
+def _(arg):
+	return arg
+
+
+# Add-on information variables
+addon_info = {"addon_name": "WhatsApp-Desktop",
+"addon_summary": "WhatsApp Desktop",
+"addon_description": """Atajos de teclado para acceder fácilmente a ciertas funciones de la aplicación.""",
+"addon_version": "0.3",
+"addon_author": "Gerardo Kessler <ReaperYOtrasYerbas@gmail.com>",
+"addon_url": "http://github.com/GerardKessler/WhatsApp-desktop",
+"addon_docFileName": "readme.html",
+"addon_minimumNVDAVersion": "2019.3.0",
+"addon_lastTestedNVDAVersion": "2021.1.0",
+"addon_updateChannel": None,
+}
+
+# Define the python files that are the sources of your add-on.
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+# For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
+# the list can be written as follows:
+# pythonSources = ["addon/globalPlugins/*.py"]
+# For more information on SCons Glob expressions please take a look at:
+# https://scons.org/doc/production/HTML/scons-user/apd.html
+pythonSources = []
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
+
+# Base language for the NVDA add-on
+# If your add-on is written in a language other than english, modify this variable.
+# For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
+baseLanguage = "en"
+
+# Markdown extensions for add-on documentation
+# Most add-ons do not require additional Markdown extensions.
+# If you need to add support for markup such as tables, fill out the below list.
+# Extensions string must be of the form "markdown.extensions.extensionName"
+# e.g. "markdown.extensions.tables" to add tables.
+markdownExtensions = []
